564fabf9c4f468633e580a27783611efef65dccb
komaji <cy11121@shibaura-it.ac.jp>
2016-10-24 01:04:46 +0900

    add sample

--- SampleUIAlertViewController/ViewController.swift
+++ SampleUIAlertViewController/ViewController.swift
@@ -10,16 +10,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let defaultAction = UIAlertAction(title: "Default",
+                                      style: .default,
+                                      handler: { action in
+                                        print("Default")
+    })
+    
+    let defaultAction2 = UIAlertAction(title: "Default2",
+                                       style: .default,
+                                       handler: { action in
+                                        print("Default2")
+    })
+    
+    let destructiveAction = UIAlertAction(title: "Destructive",
+                                          style: .destructive,
+                                          handler: { action in
+                                            print("Destructive")
+    })
+    
+    let destructiveAction2 = UIAlertAction(title: "Destructive2",
+                                          style: .destructive,
+                                          handler: { action in
+                                            print("Destructive2")
+    })
+    
+    let cancelAction = UIAlertAction(title: "Cancel",
+                                     style: .cancel,
+                                     handler: { action in
+                                        print("Cancel")
+    })
+    
+    let cancelAction2 = UIAlertAction(title: "Cancel2",
+                                     style: .cancel,
+                                     handler: { action in
+                                        print("Cancel2")
+    })
+    
+    @IBAction func alertButtonDidTap(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Alert",
+                                      message: "Alert Message",
+                                      preferredStyle: .alert
+        )
+    
+        alert.addAction(defaultAction)
+        alert.addAction(defaultAction2)
+        alert.addAction(destructiveAction)
+        alert.addAction(destructiveAction2)
+        alert.addAction(cancelAction)
+        
+        // キャンセルを2つ追加するとクラッシュする
+        //  alert.addAction(cancelAction2)
+        
+        present(alert, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func actionSheetButtonDidTap(_ sender: UIButton) {
+        let actionSheet = UIAlertController(title: "Action Sheet",
+                                      message: "Action Sheet Message",
+                                      preferredStyle: .actionSheet
+        )
+        
+        actionSheet.addAction(defaultAction)
+        actionSheet.addAction(defaultAction2)
+        actionSheet.addAction(destructiveAction)
+        actionSheet.addAction(destructiveAction2)
+        actionSheet.addAction(cancelAction)
+        
+        // キャンセルを2つ追加するとクラッシュする
+        // actionSheet.addAction(cancelAction2)
+        
+        present(actionSheet, animated: true, completion: nil)
     }
-
 
 }
 
